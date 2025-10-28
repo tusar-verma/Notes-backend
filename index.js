d3666d6d8caecc807883cbce0e266bcbd98b5771
tusar-verma <tusarverma12345@gmail.com>
2025-10-28 15:06:11 -0300

    Added front to deploy

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const unknownEndpoint = require('./middlewares/unknownEndpoint')
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
+app.use(express.static('dist'))
 
 app.use('/api/notes', notesRouter)
 
